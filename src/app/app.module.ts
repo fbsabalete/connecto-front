bcda0e28626d05925d9dbce60fd7d14e8ee43d45
Gustavo Lima <gustavo1.l@hotmail.com>
2021-03-22 16:10:05 -0300

    feat(pagina-inicial): Menu criado #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
