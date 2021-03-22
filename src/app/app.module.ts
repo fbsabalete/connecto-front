12743811895ddd2f8961c95080cbd1c099739351
NATA-GRECU <natagrecu34@gmail.com>
2021-03-22 11:05:21 -0300

    feat(saiba-mais): Criando componente saiba mais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SaibaMaisComponent } from './saiba-mais/saiba-mais.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaibaMaisComponent
   ],
   imports: [
     BrowserModule,
