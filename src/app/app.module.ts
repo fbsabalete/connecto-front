429c366a8d4c95f96ed59406b8a59ee60fbb1708
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-29 11:23:09 -0300

    feat(landing-page): Criado componente inicio #10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EquipeComponent } from './equipe/equipe.component';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { SaibaMaisComponent } from './saiba-mais/saiba-mais.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EquipeComponent,
     PaginaInicialComponent,
-    SaibaMaisComponent
+    SaibaMaisComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
