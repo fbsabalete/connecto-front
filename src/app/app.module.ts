53419352cee5365a25a229ece8c112b3147df5e2
cintralima <leobass26@gmail.com>
2021-03-30 16:21:46 -0300

    fix: importado componente e acrescentado seletores no feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 import { FeedComponent } from './feed/feed.component';
+import { PostagemComponent } from './postagem/postagem.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuLateralComponent,
-    FeedComponent
+    FeedComponent,
+    PostagemComponent
 
   ],
   imports: [
