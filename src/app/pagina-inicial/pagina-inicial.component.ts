e117e3cf27d3f7d641ea4c0f2f0a8e6327127f24
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-31 19:33:31 -0300

    feat(landing-page): Implementada função esconder navbar

--- src/app/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pagina-inicial/pagina-inicial.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaInicialComponent implements OnInit {
 
-  constructor() { }
+  clicked = false;
+  lastScroll: number = 0;
 
-  ngOnInit(): void {
+  constructor() {
+    window.addEventListener("scroll", this.hideNav)
+  }
+
+  ngOnInit() {
+  }
+
+  hideNav() {
+    let scroll = window.scrollY;
+    var bar = document.querySelector("#navegacao");
+    let logo = document.querySelector("#logo")
+    if(scroll != 0){
+        if(scroll > this.lastScroll) {
+        bar.classList.remove("scroll-up");
+        bar.classList.add("scroll-down");
+        } else {
+        bar.classList.remove("scroll-down");
+        bar.classList.add("scroll-up");
+        logo.setAttribute('src', "../../assets/img/connecto branco.svg")
+        }
+    }else{
+        bar.classList.add("scroll-min")
+        bar.classList.remove("scroll-down");
+        bar.classList.remove("scroll-up");
+        logo.setAttribute('src', "../../assets/img/connecto.svg")
+    }
+    this.lastScroll = scroll;
+  }
+
+  clickCollapse(){
+    var bar = document.querySelector("#navegacao");
+    if(!this.clicked){
+      bar.classList.add("nav-button-clicked");
+    }else{
+      bar.classList.remove("nav-button-clicked");
+    }
+    this.clicked = !this.clicked;
   }
 
 }
