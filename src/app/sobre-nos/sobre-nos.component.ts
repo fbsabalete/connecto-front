2f3e2e32cbe540cc4e755b1c0471ed45d820c64d
NATA-GRECU <natagrecu34@gmail.com>
2021-04-09 13:56:28 -0300

    feat(sobre-nos): Criando página sobre-nos #28

--- src/app/sobre-nos/sobre-nos.component.ts
+++ src/app/sobre-nos/sobre-nos.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreNosComponent implements OnInit {
 
+  clicked = false;
+
   constructor() { }
 
   ngOnInit() {
+    window.scroll(0,0)
+    }
+
+  clickCollapse(){
+    var bar = document.querySelector("#navegacao");
+    if(!this.clicked){
+      bar.classList.add("nav-button-clicked");
+    }else{
+      bar.classList.remove("nav-button-clicked");
+    }
+    this.clicked = !this.clicked;
   }
 
 }
